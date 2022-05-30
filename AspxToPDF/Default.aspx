794967d3d8a2aefc6cfb07c7df84a39409e933f5
Muhammad Aatif Shafique <aslidsp@live.com>
2022-05-31 00:28:10 +0500

    Changes

---
+++ AspxToPDF/Default.aspx
@@ -0,0 +1,38 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="AspxToPDF.Default" EnableEventValidation="false" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div style="text-align:center;">
+    <h3>This is Test Page - For Export as PDF</h3>
+    </div>
+
+        <table>
+            <tr>
+                <td>Name</td>
+                <td>Age</td>
+            </tr>
+            <tr>
+                <td>Name 1</td>
+                <td>Age 1</td>
+            </tr>
+            <tr>
+                <td>Name 2</td>
+                <td>Age 2</td>
+            </tr>
+            <tr>
+                <td>Name 3</td>
+                <td>Age 3</td>
+            </tr>
+        </table>
+
+        <asp:Button Text="Export" ID="btnExport" OnClick="btnExport_Click" runat="server" />
+
+    </form>
+</body>
+</html>
---
+++ AspxToPDF/Default.aspx.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+using iTextSharp.text;
+using iTextSharp.text.pdf;
+using iTextSharp.tool.xml;
+
+namespace AspxToPDF
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void btnExport_Click(object sender, EventArgs e)
+        {
+            using (StringWriter sw = new StringWriter())
+            {
+                using (HtmlTextWriter hw = new HtmlTextWriter(sw))
+                {
+                    this.Page.RenderControl(hw);
+                    StringReader sr = new StringReader(sw.ToString());
+                    Document pdfDoc = new Document(PageSize.A4, 10f, 10f, 10f, 10f);
+                    PdfWriter writer = PdfWriter.GetInstance(pdfDoc, Response.OutputStream);
+                    pdfDoc.Open();
+                    XMLWorkerHelper.GetInstance().ParseXHtml(writer, pdfDoc, sr);
+                    pdfDoc.Close();
+                    Response.ContentType = "application/pdf";
+                    Response.AddHeader("content-disposition","attachment;filename=file1.pdf");
+                    Response.Cache.SetCacheability(HttpCacheability.NoCache);
+                    Response.Write(pdfDoc);
+                    Response.End();
+                }
+            }
+        }
+    }
+}
